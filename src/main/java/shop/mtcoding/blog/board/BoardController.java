c38b147e4c5fde83adcd40f948fe64c74d24d42f
yeeun0118s <yeeun0118s@naver.com>
2024-02-06 16:09:11 +0900

    ê게시글 권한체크

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import shop.mtcoding.blog.user.User;
 
@@ -18,7 +19,36 @@ public class BoardController {
 
     private final BoardRepository boardRepository;
 
-    @GetMapping({ "/", "/board" })
+    @PostMapping("/board/save")
+    public String save(BoardRequest.SaveDTO requestDTO, HttpServletRequest request){
+        //1. 인증체크
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            return "redirect:/loginForm";
+        }
+
+
+
+        //2. 바디 데이터 확인 및 유효성 검사
+        System.out.println(requestDTO);
+
+        if (requestDTO.getTitle().length() > 30){
+            request.setAttribute("status",400);
+            request.setAttribute("msg","title의 길이가 30자를 초과해서는 안됩니다.");
+            return "error/40x"; //BadRequest
+        }
+
+
+        //3.모델 위임
+        //insert into board_tb(title, content, user_id, created_at) values(?,?,?, now));
+        boardRepository.save(requestDTO, sessionUser.getId());
+
+
+
+        return "redirect:/";
+    }
+
+    @GetMapping({"/", "/board"})
     public String index(HttpServletRequest request) {
 
         List<Board> boardList = boardRepository.findAll();
@@ -33,7 +63,7 @@ public class BoardController {
         User sessionUser = (User) session.getAttribute("sessionUser");
         //        값이 null이면 로그인 페이지로 redirection
         // 값이 null이 아니면 /board/saveForm으로 이동
-        if(sessionUser == null){
+        if (sessionUser == null) {
             return "redirect:/loginForm";
         }
         return "board/saveForm";
@@ -41,12 +71,27 @@ public class BoardController {
 
     @GetMapping("/board/{id}")
     public String detail(@PathVariable int id, HttpServletRequest request) {
-        System.out.println("id : "+id);
-
-        // 바디 데이터가 없으면 유효성 검사가 필요없지 ㅎ
+       //1. 모델 진입 - 상세보기 데이터 가져오기
+        // 바디 데이터가 없으면 유효성 검사가 필요없다
         BoardResponse.DetailDTO responseDTO = boardRepository.findById(id);
 
+
+
+      //2. 페이지 주인 여부 체크(board의 userId와 sessionUser의 id를 비교)
+        boolean pageOwner;
+
+        User sessionUser = (User) session.getAttribute("sessionUser");//세션에 저장 돼있는 user객체 가져오기
+
+        int boardID = responseDTO.getUserId();
+        int UserId = sessionUser.getId();
+
+        if (sessionUser == null){
+            pageOwner=false;
+        }else{
+            pageOwner = boardID == UserId;
+        }
         request.setAttribute("board", responseDTO);
+        request.setAttribute("pageOwner", pageOwner);
         return "board/detail";
     }
 }
